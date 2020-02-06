222f7bb99431e5299b53f186d5cd13f8b80ca1a7
ImBrent <clappbrent@gmail.com>
2020-02-06 16:05:30 -0600

    Added basic files to start coding

---
+++ Project_1/Source_Code/Interface.java
@@ -0,0 +1,11 @@
+/**********************************************************************
+Interface.java
+This file contains code for the user to interact with the Warehouse System via a terminal.
+***********************************************************************/
+
+public static void main(String[] args){
+
+
+
+
+}//end main
