0f2881c1e634aed479cef3ee68f2441f5a6aafaf
chrismd2 <53451424+chrismd2@users.noreply.github.com>
2020-02-11 21:44:25 -0600

    User Interface Loop Made

--- Project_1/Source_Code/UserInterface.java
+++ Project_1/Source_Code/UserInterface.java
@@ -2,9 +2,23 @@
 UserInterface.java
 This file contains code for the user to interact with the Warehouse System via a terminal.
 ***********************************************************************/
-public class UserInterface{
-	public static void main(String[] args){
-		Warehouse warehouse;
 
-	}//end main
+//package Project_1.Source_Code;
+
+import java.util.Scanner;
+
+public class UserInterface
+{
+	public static void main(String[] args){
+		//Warehouse warehouse;
+
+		Scanner input = new Scanner(System.in);
+
+		String inputStr = "notExit";
+		while(!inputStr.equals("exit") && !inputStr.equals("e")){
+			System.out.println("type exit to quit");
+			inputStr = input.next();
+		}
+
+	}//end run
 }
