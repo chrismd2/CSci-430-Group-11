5349b8b678ae4cce9cfd44a1f9decedbbb922fb5
Sabeen13 <basnets131@gmail.com>
2020-02-12 21:22:36 -0600

    Update WaitList.java

--- Project_1/Source_Code/WaitList.java
+++ Project_1/Source_Code/WaitList.java
@@ -1,7 +1,37 @@
 //package Project_1.Source_Code;
 package Source_Code;
 
-public class WaitList{
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+public class Waitlist implements Serializable {
+  private static final long serialVersionUID = 1L;
+  private Product product;
+  private Client client;
+  private int quantity;
+
+  public Waitlist(Client c, Product p, int q){
+    this.client   = c;
+    this.product  = p;
+    this.quantity = q;
+  }
+
+  public Product getProduct(){
+    return product;
+  }
+
+  public Client getClient(){
+    return client;
+  }
+
+  public int getQuantity(){
+    return quantity;
+  }
+
+  public void updateQuantity(int newQ){
+    this.quantity = newQ;
+  }
+}
 
 
 
