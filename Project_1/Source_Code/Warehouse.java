f5e76e865294dfbf8b96802584ff42e090eefc6b
ImBrent <clappbrent@gmail.com>
2020-02-13 16:15:35 -0600

    Update Warehouse.java

--- Project_1/Source_Code/Warehouse.java
+++ Project_1/Source_Code/Warehouse.java
@@ -13,15 +13,19 @@
 
 
 ******************************************************************************/
-//package Project_1.Source_Code;
 package Source_Code;
 
-public class Warehouse{
-	OrderList orders;
-	ClientList clients;
-	SupplierList suppliers;
-	ProductList products;
+import java.util.*;
+import java.io.*;
+import java.lang.*;
 
+public class Warehouse implements Serializable{
+	private OrderList orders;
+	private ClientList clients;
+	private SupplierList suppliers;
+	private ProductList products;
+	private static Warehouse warehouse;
+	
 	//Default constructor:
 	public Warehouse(){
 		//Initialize all lists to empty:
@@ -31,6 +35,15 @@ public class Warehouse{
 		products = new ProductList();
 	}//end constructor
 
+	//Instance method:
+	public static Warehouse instance(){
+		if(warehouse == null) {
+			return (warehouse = new Warehouse() );
+		} else {
+			return warehouse;
+		}//end if-else
+	}//end instance
+	
 
 	/*************************************************************
 	addClient
@@ -41,6 +54,36 @@ public class Warehouse{
 	}//end addClient
 
 
-
-
-}
+	/*************************************************************
+	Read/Write serialization methods:
+	*************************************************************/
+	private void writeObject(java.io.ObjectOutputStream output){
+		try{
+			output.defaultWriteObject();
+			output.writeObject(warehouse);
+		} catch(IOException ioe){
+			ioe.printStackTrace();
+		}//end try-catch block
+	}//end writeObject
+	
+	private void readObject(java.io.ObjectInputStream input){
+		try{
+			if(warehouse != null){
+				return;
+			} else{
+				input.defaultReadObject();
+				if(warehouse == null){
+					warehouse = (Warehouse) input.readObject();
+				} else {
+					input.readObject();
+				}//end if-else
+			}//end if-else
+		} catch (IOException ioe){
+			ioe.printStackTrace();
+		} catch(ClassNotFoundException cnfe){
+			cnfe.printStackTrace();
+		}//end try-catch block
+	
+	}//end readObject
+	/************* End Serialization methods **************************************/
+}//end Warehouse class
