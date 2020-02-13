584533e7d5ef8d74c2b0d5926fdd7ca011cc5346
Sabeen13 <basnets131@gmail.com>
2020-02-12 21:09:56 -0600

    Update Supplier.java

--- Project_1/Source_Code/Supplier.java
+++ Project_1/Source_Code/Supplier.java
@@ -1,8 +1,39 @@
 //package Project_1.Source_Code;
-package Source_Code;
 
-public class Supplier{
+import java.util.*;
+import java.lang.*;
+import java.io.*;
 
+public class Supplier implements Serializable {
+  private static final long serialVersionUID = 1L;
+  private Manufacturer manu;
+  private int quantity;
+  private double price;
 
+  public Supplier(Manufacturer m, int quantity, double price){
+    this.manu = m;
+    this.quantity = quantity;
+    this.price = price;
+  }
 
+  public Manufacturer getManufacturer()
+  {
+    return manu;
+  }
+
+  public int getQuantity()
+  {
+    return quantity;
+  }
+
+  public double getPrice()
+  {
+    return price;
+  }
+
+  public void setNewQuantity(int q)
+  {
+    this.quantity = quantity - q;
+  }
 }
+
