3e5c85e2c1d7a8b392bb39d22850bfaefb366683
Sabeen13 <basnets131@gmail.com>
2020-04-03 16:28:58 -0500

    Update ClientMenuState.java

--- Project2/ClientMenuState.java
+++ Project2/ClientMenuState.java
@@ -119,6 +119,80 @@ public class ClientMenuState{
 	 if(!productFound)
 		System.out.println("Client has no waitlisted items");
   }//end showWaitList
+	       
+	       
+/*	        //editing and adding the products in the shopping cart
+	public class shop {
+    public void main(String[] args) {
+	   
+	  Scanner sc = new Scanner(System.in);
+	    ArrayList<ItemList> cart = new ArrayList<ItemList>();
+
+	    ItemList item;
+	    int itemID;
+	    String itemName;
+	    double itemPrice;
+	    String itemDescription;
+	    int itemQuantity;
+	    double itemTax;
+	    int ch;
+	    String choice;
+
+	    ProductList shoppingCart = new ProductList();
+
+	    while (true) {
+	        System.out.println("Menu:");
+	        System.out.println("0) Exit " + "\n"
+	                + "1) Add item in shopping cart" + "\n"
+	                + "2) Remove item from shpping cart");
+	        ch = sc.nextInt();
+
+	        switch (ch) {
+	        case 0:
+	            System.out.println("\n" + "Good bye!");
+	            System.exit(0);
+
+	        case 1:
+	            System.out.println("Enter item ID: ");
+	            itemID = sc.nextInt();
+
+	            System.out.println("Enter item name: ");
+	            itemName = sc.next();
+
+	            System.out.println("Enter item price: ");
+	            itemPrice = sc.nextDouble();
+
+	            System.out.println("Enter short description of item: ");
+	            itemDescription = sc.next();
+
+	            System.out.println("Enter quantity: ");
+	            itemQuantity = sc.nextInt();
+
+	            System.out.println("Enter tax rate:");
+	            itemTax = sc.nextDouble();
+
+
+	            shoppingCart.add(itemID, itemName, itemPrice, itemDescription, itemQuantity,       itemTax);
+
+	            break;
+
+	        case 2:
+	            System.out.println("Enter name of the item that you would like to remove: ");
+	            choice = sc.next();
+	            shoppingCart.remove(choice);
+
+	            break;
+	        }
+
+	    }
+	}
+
+	}
+	       */
+	       
+	       
+	       
+	       
 
   public static void processInput(Warehouse w){
 		warehouse = w;
