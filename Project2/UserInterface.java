0885924066358f8da8e502886344ba97fb7b52d9
Sabeen13 <basnets131@gmail.com>
2020-04-02 23:59:08 -0500

    Update UserInterface.java

--- Project2/UserInterface.java
+++ Project2/UserInterface.java
@@ -3,13 +3,20 @@ import java.io.*;
 import java.lang.*;
 
 public class UserInterface {
+	final static String MAINMENU =  ""+
+	        "SELECT STATE          \n\t"+
+	          "1. Client Menu State\n\t"+
+	          "2. Clerk Menu State\n\t"+
+	          "3. Manager Menu State\n\n";
+
   public static void main(String args[]){
-			//Creating warehouse
+			//Creating warehouse	  
       Scanner s = new Scanner(System.in);
+      System.out.println(MAINMENU);
       String choice = s.nextLine();
       switch(choice){
         case "1":
-          System.out.println("ERROR: client operations are unavailable at this time");
+          ClientMenuState.processInput();
           break;
         case "2":
           ClerkMenuState.processInput();
@@ -23,3 +30,4 @@ public class UserInterface {
       }//end switch
 	}//end main
 }
+
